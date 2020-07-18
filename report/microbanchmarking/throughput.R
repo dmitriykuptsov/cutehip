170f8c7db29363565f6755b9f068d047e3f434a9
dmitriykuptsov <dmitriy@1000melochey.uz>
2020-07-18 21:34:08 +0500

    Update paper

---
+++ report/microbanchmarking/throughput.R
@@ -0,0 +1,8 @@
+ipsec<-scan("throughput_ipsec.dat")
+no_ipsec<-scan("throughput_no_ipsec.dat")
+pdf("throughput.pdf", height=4);
+plot(ecdf(ipsec), col="dark red", lwd=6, main="", ylab="Probability", xlab="Throughput, Mb/s", log="x", verticals=T, do.points=F, xlim=c(1, 42));
+plot(ecdf(no_ipsec), col="dark blue", lwd=6, add=T, verticals=T, do.points=F);
+grid(col="black");
+legend("bottomright", c("IPSec", "Plain TCP"), col=c("dark red", "dark blue"), bty="n", lwd=c(3, 3));
+dev.off();
