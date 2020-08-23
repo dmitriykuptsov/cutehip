04672d264ff4780992c7ab0077a5492df94764fe
dmitriykuptsov <dmitriy@1000melochey.uz>
2020-08-23 13:09:56 +0500

    SHA-2 AES for ESP; updated keys

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ config = {
 	"security": {
 		"public_key": "./config/public.pem",                   # ECDSA/RSA public key
 		"private_key": "./config/private.pem",                 # ECDSA/RSA private key
-		"sig_alg": 0x7,                                        # RSA 5, ECDSA 7, ECDSA LOW 9, DSA 3
-		"hash_alg": 0x2,                                       # SHA-256 0x1, SHA-384 0x2, SHA-1 0x3
+		"sig_alg": 0x5,                                        # RSA 5, ECDSA 7, ECDSA LOW 9, DSA 3
+		"hash_alg": 0x1,                                       # SHA-256 0x1, SHA-384 0x2, SHA-1 0x3
 		# If signature algorithm is ECDSA,
 		# then HASH algorithm should be SHA-384,
 		# HASH algorithm is the one that is used
@@ -21,7 +21,7 @@ config = {
 		"supported_hit_suits": [0x10, 0x20, 0x30],             # SHA256 (0x1), SHA384 (0x2), SHA1 (0x3)
 		"supported_transports": [0x0FFF],                      # IPSec
 		"supported_signatures": [0x5, 0x7, 0x9],               # DSA (0x3), RSA (0x5), ECDSA (0x7), ECDSA_LOW (0x9)
-		"supported_esp_transform_suits": [0x7, 0x8, 0x9]        # NULL with HMAC-SHA-256 (0x7), AES-128-CBC with HMAC-SHA-256 (0x8), AES-256-CBC with HMAC-SHA-256 (0x9)
+		"supported_esp_transform_suits": [0x9, 0x8, 0x7]        # NULL with HMAC-SHA-256 (0x7), AES-128-CBC with HMAC-SHA-256 (0x8), AES-256-CBC with HMAC-SHA-256 (0x9)
 	},
 	"resolver": {
 		"hosts_file": "./config/hosts",
